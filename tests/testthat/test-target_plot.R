30079324492e09046612d85e137d00c3fb0eb73c
isakro <isak.hkr@hotmail.com>
2023-02-23 13:04:06 +0100

    Renamed and re-did snapshots for test-target_plot.R

--- tests/testthat/test-target_plot.R
+++ tests/testthat/test-target_plot.R
@@ -9,7 +9,7 @@ test_that("returns expected plot when a target is passed", {
   set.seed(123) # For label placement
   target_point <- sf::st_sfc(sf::st_point(c(579570, 6582982)), crs = 32632)
   p <- target_plot(target_point)
-  vdiffr::expect_doppelganger("plot with a target point", p)
+  vdiffr::expect_doppelganger("target point", p)
 })
 
 test_that("returns expected plot in greyscale when a target is passed", {
@@ -17,7 +17,7 @@ test_that("returns expected plot in greyscale when a target is passed", {
   set.seed(123) # For label placement
   target_point <- sf::st_sfc(sf::st_point(c(579570, 6582982)), crs = 32632)
   p <- target_plot(target_point, greyscale = TRUE)
-  vdiffr::expect_doppelganger("greyscale plot", p)
+  vdiffr::expect_doppelganger("greyscale", p)
 })
 
 test_that("gives warning if one or more points are located outside the study area", {
